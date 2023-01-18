15611cb09d1b04043d4f8f272ba0fce4a07f23b5
peedroca <phm1080@gmail.com>
2023-01-18 12:43:55 -0300

    Added: Server and Web

--- web/src/components/Habit/index.tsx
+++
@@ -1,9 +0,0 @@
-interface HabitProps {
-    completed: number
-}
-
-export function Habit(props: HabitProps) {
-    return (
-        <p className="bg-zinc-900 h-10 w-10 m-2 rounded flex items-center justify-center text-white">{props.completed}</p>
-    )
-}
